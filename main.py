a5bc2277fe44642c0ccc791e1723db325cb047e8
will7hughes <will7hughes@gmail.com>
2019-05-28 00:31:12 +0000

    Initial

---
+++ main.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+led=11
+GPIO.setup(led, GPIO.LOW, initial=GPIO.LOW)
+
+try:
+	while(True):
+		GPIO.output(led, GPIO.HIGH)
+		print("ON")
+		time.sleep(1)
+		GPIO.output(led, GPIO.LOW)
+		print("OFF")
+		time.sleep(1)
+except KeyboardInterrupt:
+	print("Keyboard interrupt")
+finally:
+	print("Cleaning up and exiting")
+	GPIO.cleanup()
